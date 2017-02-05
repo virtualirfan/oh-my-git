f7c48af55037ef184850967655ead714b85704ae
Irfan Ahmad <irfan@cloudphysics.com>
2017-02-05 01:56:20 -0800

    Add configurability to the dark color scheme via environment variable.

--- prompt.sh
+++ prompt.sh
@@ -30,6 +30,7 @@ if [ -n "${BASH_VERSION}" ]; then
     : ${omg_default_color_on:='\[\033[1;37m\]'}
     : ${omg_default_color_off:='\[\033[0m\]'}
     : ${omg_last_symbol_color:='\e[0;31m\e[40m'}
+    : ${omg_dark_background:='\[\033[48;5;1m\]'}
     
     PROMPT='$(build_prompt)'
     RPROMPT='%{$reset_color%}%T %{$fg_bold[white]%} %n@%m%{$reset_color%}'
@@ -100,7 +101,12 @@ if [ -n "${BASH_VERSION}" ]; then
         local red_on_black="${red}${background_black}"
         local black_on_red="${black}${background_red}"
         local white_on_red="${white}${background_red}"
+        local black_on_yellow="${black}${background_yellow}"
+        local white_on_yellow="${white}${background_yellow}"
         local yellow_on_red="${yellow}${background_red}"
+
+        local black_on_dark="${black}${omg_dark_background}"
+        local white_on_dark="${white}${omg_dark_background}"
 
 
         # Flags
@@ -128,13 +134,13 @@ if [ -n "${BASH_VERSION}" ]; then
 
             # where
 
-            prompt="${prompt} ${white_on_red} ${black_on_red}"
+            prompt="${prompt} ${white_on_dark} ${black_on_dark}"
             if [[ $detached == true ]]; then
-                prompt+=$(enrich_append $detached $omg_detached_symbol "${white_on_red}")
-                prompt+=$(enrich_append $detached "(${current_commit_hash:0:7})" "${black_on_red}")
+                prompt+=$(enrich_append $detached $omg_detached_symbol "${white_on_dark}")
+                prompt+=$(enrich_append $detached "(${current_commit_hash:0:7})" "${black_on_dark}")
             else            
                 if [[ $has_upstream == false ]]; then
-                    prompt+=$(enrich_append true "-- ${omg_not_tracked_branch_symbol}  --  (${current_branch})" "${black_on_red}")
+                    prompt+=$(enrich_append true "-- ${omg_not_tracked_branch_symbol}  --  (${current_branch})" "${black_on_dark}")
                 else
                     if [[ $will_rebase == true ]]; then
                         local type_of_upstream=$omg_rebase_tracking_branch_symbol
@@ -143,23 +149,23 @@ if [ -n "${BASH_VERSION}" ]; then
                     fi
 
                     if [[ $has_diverged == true ]]; then
-                        prompt+=$(enrich_append true "-${commits_behind} ${omg_has_diverged_symbol} +${commits_ahead}" "${white_on_red}")
+                        prompt+=$(enrich_append true "-${commits_behind} ${omg_has_diverged_symbol} +${commits_ahead}" "${white_on_dark}")
                     else
                         if [[ $commits_behind -gt 0 ]]; then
-                            prompt+=$(enrich_append true "-${commits_behind} ${white_on_red}${omg_can_fast_forward_symbol}${black_on_red} --" "${black_on_red}")
+                            prompt+=$(enrich_append true "-${commits_behind} ${white_on_dark}${omg_can_fast_forward_symbol}${black_on_dark} --" "${black_on_dark}")
                         fi
                         if [[ $commits_ahead -gt 0 ]]; then
-                            prompt+=$(enrich_append true "-- ${white_on_red}${omg_should_push_symbol}${black_on_red}  +${commits_ahead}" "${black_on_red}")
+                            prompt+=$(enrich_append true "-- ${white_on_dark}${omg_should_push_symbol}${black_on_dark}  +${commits_ahead}" "${black_on_dark}")
                         fi
                         if [[ $commits_ahead == 0 && $commits_behind == 0 ]]; then
-                            prompt+=$(enrich_append true " --   -- " "${black_on_red}")
+                            prompt+=$(enrich_append true " --   -- " "${black_on_dark}")
                         fi
                         
                     fi
-                    prompt+=$(enrich_append true "(${current_branch} ${type_of_upstream} ${upstream//\/$current_branch/})" "${black_on_red}")
+                    prompt+=$(enrich_append true "(${current_branch} ${type_of_upstream} ${upstream//\/$current_branch/})" "${black_on_dark}")
                 fi
             fi
-            prompt+=$(enrich_append ${is_on_a_tag} "${omg_is_on_a_tag_symbol} ${tag_at_current_commit}" "${black_on_red}")
+            prompt+=$(enrich_append ${is_on_a_tag} "${omg_is_on_a_tag_symbol} ${tag_at_current_commit}" "${black_on_dark}")
             prompt+="${omg_last_symbol_color}${reset}\n"
             prompt+="$(eval_prompt_callback_if_present)"
             prompt+="${omg_second_line}"
